465d8850fdbdb56cd76c09c7be6580d5af53d2d9
AbhiramRavula <138038324+AbhiramRavula@users.noreply.github.com>
2025-05-06 16:39:10 +0000

    done i guess

--- graphql-server-example_js/index.js
+++ graphql-server-example_js/index.js
@@ -45,6 +45,30 @@ const resolvers = {
         game(parent){
             return db.games.find((game) => game.id === parent.game_id);
         }
+    },
+    Mutation: {
+        deleteGame(_, args){
+            db.games = db.games.filter((game) => game.id !== args.id);
+            return db.games;
+        },
+        addGame(_, args){
+            let game ={
+                ...args.game, 
+                id: Math.floor(Math.random()*10000).toString()
+            }
+            db.games.push(game)
+            return game;
+        },
+        updateGame(_, args){
+            db.games = db.games.map((g) => {
+                if(g.id === args.id){
+                    return { ...g, ...args.edits}
+                    return g;
+                }
+
+                return db.games.find((g)=> g.id === args.id);
+            }
+        )}
     }
 }
 
